6a0cc74f32ece836c0c8fe740a029cf045fc51b2
kunesv <kunesv@sprinters.cz>
2023-07-06 14:41:58 +0200

    Cleanup of svelte defaults

--- svelte-app/src/App.svelte
+++ svelte-app/src/App.svelte
@@ -26,17 +26,11 @@
   </Router>
 </div> -->
 
-<svelte:head>
-  <meta charset="utf-8"/>
-  <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no"/>
-  <title>Bill SPRINTER and Sons</title>
-  <link rel="stylesheet" href="index.css"/>
-  <link rel="icon" type="image/png" href="bs-logo-png.png"/>
-</svelte:head>
-
 <section id="top-bar">
     <h1 id="bill-sprinters">
         <img src="bs-logo2.svg" alt=""/>            
     </h1>
+</section>
+<section id="image-bar">
+
 </section>
